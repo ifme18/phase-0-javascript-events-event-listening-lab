0f9195e29ad0b3e879586fdc252c0647f24991c4
ifme18 <mainakenken188@gmail.com>
2024-09-23 08:00:52 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Grab the input element by its ID 'button'
+    const input = document.getElementById('button');
+    
+    // Create a named function to handle the click event
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add an event listener to the input element
+    input.addEventListener('click', clickAlert);
+  }
+  
